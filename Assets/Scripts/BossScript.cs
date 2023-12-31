1db12921220935b80739a05b2e91313e88c7b863
Skillcheese <34143022+Skillcheese@users.noreply.github.com>
2023-12-31 15:39:49 -0600

    basic boss script

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -8,16 +8,37 @@ public class BossScript : MonoBehaviour
     private DamageProjectile BossProjectilePrefab;
     void Start()
     {
-        StartCoroutine(DoShootingCircle(8, 0, 0));
+        StartCoroutine(BossScriptCoroutine());
     }
 
-    private IEnumerator DoShootingCircle(int _NumProjectiles, float _CircleAngleForward, float _CircleAngleRight)
+    private IEnumerator BossScriptCoroutine()
+    {
+        float shotDelay = .01f;
+        int numProjectiles = 35;
+        int numCircles = 14;
+        StartCoroutine(DoShootingCircles(numProjectiles, numCircles, numProjectiles * shotDelay, shotDelay));
+        yield break;
+    }
+
+
+    private IEnumerator DoShootingCircles(int _NumProjectiles, int _NumCircles, float _CircleDelay, float _ShotDelay)
+    {
+        float CircleAngleRight = 0;
+        while (CircleAngleRight < 360)
+        {
+            StartCoroutine(DoShootingCircle(_NumProjectiles, 0, CircleAngleRight, _ShotDelay));
+            CircleAngleRight += 360.0f / _NumCircles;
+            yield return new WaitForSeconds(_CircleDelay);
+        }
+    }
+
+    private IEnumerator DoShootingCircle(int _NumProjectiles, float _CircleAngleForward, float _CircleAngleRight, float _ShotDelay)
     {
         float CircleAngleUp = 0;
         Quaternion rotationBase = transform.rotation;
         rotationBase *= Quaternion.AngleAxis(_CircleAngleForward, Vector3.forward);
         rotationBase *= Quaternion.AngleAxis(_CircleAngleRight, Vector3.right);
-        while(CircleAngleUp <= Mathf.PI * 2)
+        while(CircleAngleUp < 360)
         {
             DamageProjectile projectile = Instantiate(BossProjectilePrefab);
             projectile.transform.position = transform.position;
@@ -25,8 +46,8 @@ public class BossScript : MonoBehaviour
             projectileRotation *= Quaternion.AngleAxis(CircleAngleUp, Vector3.up);
             projectile.transform.rotation = projectileRotation;
             projectile.InitProjectile(gameObject, projectile.transform.forward);
-            CircleAngleUp += Mathf.PI * 2 / _NumProjectiles;
-            yield return new WaitForSeconds(1);
+            CircleAngleUp += 360.0f / _NumProjectiles;
+            yield return new WaitForSeconds(_ShotDelay);
         }
     }
 }
